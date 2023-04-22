0814052dd37d4176c33d8985d923dd89174f363f
Bhushan <103170621+GamerBhushan@users.noreply.github.com>
2023-04-22 09:45:20 +0530

    Update OTP Generator.py

--- OTP Generator/OTP Generator.py
+++ OTP Generator/OTP Generator.py
@@ -5,6 +5,6 @@ def Generate_OTP():
 	return random.randint(111111,999999)
 	
 if __name__ == "__main__":
-	for i in range(10964725):
+	for i in range(10):
 		print(f"Your OTP Is : {Generate_OTP()}\n")
 		time.sleep(1)
